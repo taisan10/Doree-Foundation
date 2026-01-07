24cc016acb5ef38e2162ae32e23933261a415722
taisan10 <tarunbansalsgnr@gmail.com>
2026-01-07 15:17:54 +0530

    first commit

--- Frontend/src/Components/About/Our-VisionMission.jsx
+++ Frontend/src/Components/About/Our-VisionMission.jsx
@@ -1,12 +1,12 @@
 export default function VisionMission() {
   return (
-    <section className="w-full max-w-7xl mx-auto py-14 px-6 lg:px-10">
+    <section className="w-full max-w-7xl mx-auto py-14 px-6 lg:px- place-items-center">
 
       {/* ROW 1 */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 mb-10 sm:mb-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 mb-10 sm:mb-0 w-110 ">
 
         {/* LEFT — TEXT */}
-        <div className="flex flex-col justify-center bg-purple-200 p-10 ">
+        <div className="flex flex-col justify-center items-center bg-purple-200 p-10 h-80">
           <h2 className="text-3xl font-bold text-center lg:text-left">
             Our Vision
           </h2>
@@ -22,25 +22,25 @@ export default function VisionMission() {
           <img
             src="/About/VisionMission/Our Vision.jpg"
             alt="Vision"
-            className="w-full h-full object-cover"
+             className="w-110 sm:w-full h-90 sm:h-full object-cover"
           />
         </div>
       </div>
 
       {/* ROW 2 */}
-      <div className="grid grid-cols-1 sm:grid-cols-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 w-110">
 
         {/* LEFT — IMAGE */}
-        <div className="h-96">
+        <div className="h-96 order-2 sm:order-1">
           <img
             src="/About/VisionMission/Our Mission.jpg"
             alt="Mission"
-            className="w-full h-full object-cover"
+             className="w-110 sm:w-full h-90 sm:h-full object-cover"
           />
         </div>
 
         {/* RIGHT — TEXT */}
-        <div className="flex flex-col justify-center bg-pink-200 p-10">
+        <div className="flex flex-col order-1 justify-center bg-pink-200 p-10 h-80 ">
           <h2 className="text-3xl font-bold text-center lg:text-left">
             Our Mission
           </h2>
