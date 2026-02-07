15da0b7a4ff222ef2a86409acf8a51520115d2aa
taisan10 <tarunbansalsgnr@gmail.com>
2026-02-07 16:22:03 +0530

    first commit

--- Frontend/src/Components/Who We Are/Bhai.jsx
+++ Frontend/src/Components/Who We Are/Bhai.jsx
@@ -752,7 +752,7 @@ export default function HowWeWorkSection() {
           <p className="text-[#131271]/80 text-base md:text-lg leading-relaxed">
             Our approach is rooted in empathy, collaboration, and accountability —
             ensuring every initiative creates real and lasting impact.
-            
+            Our approach 
 
           </p>
         </div>
